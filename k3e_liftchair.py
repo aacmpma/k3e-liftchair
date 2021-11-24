99c1bacffb9afeb824c7e01bc2740512197d0a82
AACM <acordoba@adisispma.com>
2021-11-24 00:05:06 -0500

    Add stop motor switch edge

--- k3e_liftchair.py
+++ k3e_liftchair.py
@@ -12,6 +12,7 @@ from adafruit_ina219 import ADCResolution, BusVoltageRange, INA219
 
 # CONS BEEP TYPES
 B_MOT = 1
+B_EDG = 2
 
 # CONS EVENTS
 E_SIG = 'KILL'
@@ -21,12 +22,12 @@ E_INI = 'INIT'
 
 E_LOW = 'LOW'
 E_HIGH = 'HIGH'
-E_RISING = "RISING"
-E_FALLING = "FALLING"
+E_RISING = 'RISING'
+E_FALLING = 'FALLING'
 
-E_CURRENT = "CURRENT"
-E_CHARGING = "CHARGING"
-E_NOCURRENT = "NO CURRENT"
+E_CURRENT = 'CURRENT'
+E_CHARGING = 'CHARGING'
+E_NOCURRENT = 'NO CURRENT'
 
 # CONS GPIO DEVICES
 P_IRS = 4
@@ -47,6 +48,7 @@ P_SEC = 27
 logging.basicConfig(filename=os.path.splitext(os.path.basename(__file__))[0]+".log", level=logging.DEBUG, format='%(asctime)s - %(levelname)s (%(funcName)s::%(threadName)s[%(lineno)d]): %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
 logging.info("")
 logging.info("************************* " + E_INI + " *************************")
+
 
 # Database creation pool and global variable
 def create_connection_pool():
@@ -98,6 +100,10 @@ def setup():
     GPIO.setup(P_REN, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(P_LEN, GPIO.OUT, initial=GPIO.LOW)
 
+    # Enable/Disable Motor
+    GPIO.output(P_LEN, False)
+
+
 # Cleanup GPIO and DB connection
 def cleanup():
     logging.info(E_END)
@@ -110,6 +116,7 @@ def cleanup():
     except:
         logging.debug("***")
 
+
 # Catch kill signal
 def signal_term_handler(signal, frame):
     logging.info(E_SIG)
@@ -117,14 +124,38 @@ def signal_term_handler(signal, frame):
     cleanup()
     sys.exit(1)
 
+
 # Beep types
 def beep(channel, type):
+    wait_on1 = 0
+    wait_off1 = 0
+    wait_on2 = 0
+    wait_off2 = 0
+    wait_on3 = 0
+
     if type == B_MOT:
-        GPIO.output(P_BU1, GPIO.HIGH)
-        logging.debug("Beep in channel %s type: %s", channel, type)
-        time.sleep(0.250)
-        GPIO.output(P_BU1, GPIO.LOW)
-        logging.debug("Beep in channel %s off", channel)
+        wait_on1 = 0.250
+    if type == B_EDG:
+        wait_on1 = 0.250
+        wait_off1 = 0.250
+        wait_on2 = 0.125
+        wait_off2 = 0.125
+        wait_on3 = 0.125
+
+    logging.debug("Beep in channel %s type: %s", channel, type)
+    GPIO.output(channel, GPIO.HIGH)
+    time.sleep(wait_on1)
+    GPIO.output(channel, GPIO.LOW)
+    time.sleep(wait_off1)
+    GPIO.output(channel, GPIO.HIGH)
+    time.sleep(wait_on2)
+    GPIO.output(channel, GPIO.LOW)
+    time.sleep(wait_off2)
+    GPIO.output(channel, GPIO.HIGH)
+    time.sleep(wait_on3)
+    GPIO.output(channel, GPIO.LOW)
+    logging.debug("Beep in channel %s off", channel)
+
 
 # Insert into the table the event
 def add_event(device, type, channel=0):
@@ -139,6 +170,7 @@ def add_event(device, type, channel=0):
     except mariadb.PoolError as e:
         logging.error(f"DB pool: {e}")
 
+
 # Insert into the table the charging values
 def add_charging(device, vin, vout, sv, sc, pw, pr):
     try: 
@@ -152,8 +184,23 @@ def add_charging(device, vin, vout, sv, sc, pw, pr):
     except mariadb.PoolError as e:
         logging.error(f"DB pool: {e}")
 
+
+# Edge switch
+def edge_switch(channel):
+    pin_name = None
+    if channel == P_SEU:
+        pin_name == "P_SEU"
+    elif channel == P_SEC:
+        pin_name == "P_SEC"
+    elif channel == P_SED:
+        pin_name == "P_SED"
+    logging.debug(E_RISING + " Channel: %s", channel)
+    add_event(pin_name, E_RISING, channel)
+
+
 # Move Motor
 def move_motor(channel):
+    pin_ena = P_REN
     if channel == P_SWU:
         pin = P_LPW
         pin_name = "P_LPW"
@@ -163,22 +210,39 @@ def move_motor(channel):
 
     beep(P_BU1, B_MOT)
 
-    logging.debug(E_RISING + ' Channel: %s', channel)
-    logging.debug("Values pins enable R: %s - L: %s", GPIO.input(P_REN), GPIO.input(P_LEN))
+    logging.debug(E_RISING + " Channel: %s", channel)
+    logging.debug("Values pins enable >> R: %s - L: %s", GPIO.input(P_REN), GPIO.input(P_LEN))
+    logging.debug("Values pins switch edge >> U: %s - C: %s - D: %s", GPIO.input(P_SEU), GPIO.input(P_SEC), GPIO.input(P_SED))
     add_event(pin_name, E_RISING, channel)
 
+    GPIO.output(pin_ena, True)
     while GPIO.input(channel):
+        if GPIO.input(P_SEU) and pin == P_SWU:
+            beep(P_BU1, B_EDG)
+            break
+        if GPIO.input(P_SED) and pin == P_SWD:
+            beep(P_BU1, B_EDG)
+            break
         GPIO.output(pin, True)    
         time.sleep(0.1)
-    else:
-        GPIO.output(pin, False)
-        logging.debug(E_LOW + ' Channel: %s', channel)
-        add_event(pin_name, E_LOW, channel)
+
+    GPIO.output(pin_ena, False)
+    time.sleep(0.1)
+    GPIO.output(pin, False)
+    logging.debug(E_LOW + " Channel: %s", channel)
+    add_event(pin_name, E_LOW, channel)
+
 
 # Check Battery
 def check_battery():
+    num = 0
+    wait = 5
+    interval = 5
+    event = None
+    last_event = None
     pin = P_SCH
     pin_name = "P_SCH"
+    
     i2c_bus = board.I2C()
     logging.debug("I2C BUS: %s", board.I2C())
 
@@ -187,10 +251,31 @@ def check_battery():
     ina219.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
 
     while True:
+        ++num
         bus_voltage = ina219.bus_voltage  # voltage on V- (load side)
         shunt_voltage = ina219.shunt_voltage  # voltage between V+ and V- across the shunt
         current = ina219.current/1000  # current in mA
         power = ina219.power  # power in watts
+
+        if bus_voltage+shunt_voltage >= 30 and current <= 0.01:
+            event = E_CURRENT
+        elif bus_voltage+shunt_voltage <= 30 and current <= 0.01:
+            event = E_NOCURRENT
+        elif bus_voltage+shunt_voltage <= 30 and current > 0.5:
+            event = E_CHARGING
+
+        if event != last_event:
+            num = 0
+            last_event = event
+            logging.debug(pin_name + " State change: " + event)
+            # Add event record
+            add_event(pin_name, event, pin)
+            # Add charging record
+            add_charging(pin_name, bus_voltage+shunt_voltage, bus_voltage, shunt_voltage, current, bus_voltage*current, power)
+        elif event == last_event and event != E_CURRENT and interval <= wait*num:
+            num = 0
+            # Add charging record
+            add_charging(pin_name, bus_voltage+shunt_voltage, bus_voltage, shunt_voltage, current, bus_voltage*current, power)
 
         # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
         logging.debug(
@@ -201,24 +286,13 @@ def check_battery():
             " | PW:{:8.5f}W".format(bus_voltage*current) +
             " | PR:{:6.3f}W".format(power)
             )
-        event = "NO VALUE"
-        if bus_voltage+shunt_voltage >= 30 and current <= 0.01:
-            event = E_CURRENT
-        elif bus_voltage+shunt_voltage <= 30 and current <= 0.01:
-            event = E_NOCURRENT
-        elif bus_voltage+shunt_voltage <= 30 and current > 0.5:
-            event = E_CHARGING
-        add_event(pin_name, event, pin)
-        logging.debug(pin_name + " Charging state: " + event)
-
-        # Add charging record
-        add_charging(pin_name, bus_voltage+shunt_voltage, bus_voltage, shunt_voltage, current, bus_voltage*current, power)
 
         # Check internal calculations haven't overflowed (doesn't detect ADC overflows)
         if ina219.overflow:
             logging.error("I2C Internal Math Overflow")
 
-        time.sleep(5)
+        time.sleep(wait)
+
 
 # ============== MAIN ==============
 if __name__ == "__main__":
@@ -239,13 +313,13 @@ if __name__ == "__main__":
         logging.debug("Adding GPIO event detection")
         GPIO.add_event_detect(P_SWD, GPIO.RISING, callback = move_motor, bouncetime = 200)
         GPIO.add_event_detect(P_SWU, GPIO.RISING, callback = move_motor, bouncetime = 200)
+        GPIO.add_event_detect(P_SEU, GPIO.RISING, callback = edge_switch, bouncetime = 200)
+        GPIO.add_event_detect(P_SEC, GPIO.RISING, callback = edge_switch, bouncetime = 200)
+        GPIO.add_event_detect(P_SED, GPIO.RISING, callback = edge_switch, bouncetime = 200)
 
         logging.debug("Start thead check battery")
         tcb = threading.Thread(target=check_battery, args=())
         tcb.start()
-
-        GPIO.output(P_REN, True)
-        GPIO.output(P_LEN, True)
 
         while True:
             now = datetime.now()
